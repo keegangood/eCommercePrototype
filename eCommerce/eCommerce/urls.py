e82a0518ea5adc23bb4376aa508298215ab723bf
unknown <keegood8@gmail.com>
2020-03-23 13:12:26 -0700

    fix pipenv

--- eCommerce/eCommerce/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import router
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('inventory.urls'))
-]
